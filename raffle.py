548eaaf4c0498cd8e8e87466e8056d1307f9a7dd
Andres Romanos <andres@andresromanos.com>
2018-12-29 12:29:59 +0100

    Added exclusions

--- raffle.py
+++ raffle.py
@@ -1,19 +1,36 @@
 import random
 
-children = {1: 'nacho', 2: 'elena', 3: 'alonso', 4: 'julia', 5: 'irene'}
+children = {1: 'Nacho', 2: 'Elena', 3: 'Alonso', 4: 'Julia', 5: 'Irene'}
 pairs = ['Pablo & MJ', 'Andres & Julia', 'Andres & Ana', 'Clara & Nacho', 'Jaime & Marina']
-winners = []
+exclusions = {'Pablo & MJ': ['Elena', 'Irene'], 'Clara & Nacho': ['Nacho', 'Alonso'], 'Jaime & Marina': ['Julia'], 'Andres & Julia': [], 'Andres & Ana': []}
+results = {}
 
-for pair in pairs:
-    
+def throwdice():
     dice = random.randint(1, 5)
-    winner = children[dice]
-    while winner in winners:
-        print (winner, ' is already selected, throwing the dice again')
-        dice = random.randint(1, 5)
-        winner = children[dice]
+    return children[dice]
 
-    winners.append(winner)
-    print(pair, " -> ", winner)
+def run():
+    winners = []
+    for pair in pairs:
+        winner = throwdice()
+        while winner in winners:
+            #print (winner, 'is already selected, throwing the dice again')
+            winner = throwdice()
+        while winner in exclusions[pair]:
+            #print (winner, 'is excluded for pair', pair, ', throwing the dice again')
+            winner = throwdice()
 
-print(winners)
+        winners.append(winner)
+        #print(pair, " -> ", winner)
+        results.update({pair: winner})
+
+    print('=>>', winners)
+    return tuple(winners)
+
+#combined = set()
+#for x in range(100):
+#    combined.add(run())
+#print(combined)
+
+run()
+print(results)
