5cd0e60866dfa84d4f9f66825f73274a2c793fc6
Andres Romanos <andres@andresromanos.com>
2019-12-06 17:56:52 +0100

    Added Gonzalo and Adults raffle

--- raffle.py
+++ raffle.py
@@ -1,36 +1,79 @@
 import random
 
-children = {1: 'Nacho', 2: 'Elena', 3: 'Alonso', 4: 'Julia', 5: 'Irene'}
-pairs = ['Pablo & MJ', 'Clara & Nacho', 'Jaime & Marina', 'Andres & Julia', 'Andres & Ana']
-exclusions = {'Pablo & MJ': ['Elena', 'Irene'], 'Clara & Nacho': ['Nacho', 'Alonso'], 'Jaime & Marina': ['Julia'], 'Andres & Julia': [], 'Andres & Ana': []}
-results = {}
+# Adults
+adults = ['Papa', 'Mama', 'Andres', 'Pablo', 'Clara', 'Jaime', 'Ana', 'MJ', 'Nacho', 'Marina']
+adults_exclusions = {'Pablo': ['Pablo', 'MJ'], 'Clara': ['Clara', 'Nacho'], 'Jaime': ['Jaime', 'Marina'], 'Papa': ['Papa', 'Mama'], 'Andres': ['Andres', 'Ana'],
+                        'MJ': ['MJ', 'Pablo'], 'Nacho': ['Nacho', 'Clara'], 'Marina': ['Marina', 'Jaime'], 'Mama': ['Mama', 'Papa'], 'Ana': ['Ana', 'Andres']}
+adults_results = {}
 
-def throwdice():
-    dice = random.randint(1, 5)
+# Pairs & Children
+children = {1: 'Nacho', 2: 'Elena', 3: 'Alonso', 4: 'Julia', 5: 'Irene', 6: 'Gonzalo'}
+pairs = ['Pablo & MJ', 'Clara & Nacho', 'Jaime & Marina', 'Papa & Mama', 'Andres & Ana']
+children_exclusions = {'Pablo & MJ': ['Elena', 'Irene'], 'Clara & Nacho': ['Nacho', 'Alonso'], 'Jaime & Marina': ['Julia', 'Gonzalo'], 'Papa & Mama': [], 'Andres & Ana': []}
+children_results = {}
+
+def throwdice_adults():
+    dice = random.randint(0, 9)
+    return adults[dice]
+
+def throwdice_children():
+    dice = random.randint(1, 6)
     return children[dice]
 
-def run():
+def throwdice_pairs():
+    dice = random.randint(0, 4)
+    return pairs[dice]
+
+def run_adults():
     winners = []
-    for pair in pairs:
-        winner = throwdice()
+    # Loop through Adults, each will get another adult assigned
+    for adult in adults:
+        winner = throwdice_adults()
         while winner in winners:
-            print (winner, 'is already selected, throwing the dice again')
-            winner = throwdice()
-        while winner in exclusions[pair]:
-            print (winner, 'is excluded for pair', pair, ', throwing the dice again')
-            winner = throwdice()
+            print(f'{winner} is already assigned, throwing the dice again')
+            winner = throwdice_adults()
+        while winner in adults_exclusions[adult]:
+            print(f'{winner} is excluded for adult {adult}, throwing the dice again')
+            winner = throwdice_adults()
 
         winners.append(winner)
-        print('==>', pair, ' assigned to', winner)
-        results.update({pair: winner})
+        print(f'==> {adult} assigned to {winner}')
+        adults_results.update({adult: winner})
 
-    #print('=>>', winners)
-    return tuple(winners)
+def run_children():
+    winners = []
+    # Loop through pairs, each will get a child assigned
+    for pair in pairs:
+        winner = throwdice_children()
+        while winner in winners:
+            print(f'{winner} is already assigned, throwing the dice again')
+            winner = throwdice_children()
+        while winner in children_exclusions[pair]:
+            print(f'{winner} is excluded for pair {pair}, throwing the dice again')
+            winner = throwdice_children()
 
-#combined = set()
-#for x in range(100):
-#    combined.add(run())
-#print(combined)
+        winners.append(winner)
+        print(f'==> {pair} assigned to {winner}')
+        children_results.update({pair: winner})
+
+    # One extra random pair gets a second child assigned
+    extra_pair = throwdice_pairs()
+    winner = throwdice_children()
+    while winner in winners:
+        print(f'{winner} is already assigned, throwing the dice again')
+        winner = throwdice_children()
+    while winner in children_exclusions[extra_pair]:
+        print(f'{winner} is excluded for pair {extra_pair}, throwing the dice again')
+        winner = throwdice_children()
+
+    print(f'==> {extra_pair} assigned to {winner}')
+    children_results.update({extra_pair + ' extra': winner})
+
+def run():
+    run_adults()
+    print(f'==> Adults Results: {adults_results}')
+
+    run_children()
+    print(f' Children Results: {children_results}')
 
 run()
-print(results)
