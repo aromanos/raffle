4b2f126b6406c163687524bda2faa50f8082ca44
andres <andres@andresromanos.com>
2022-12-21 12:31:30 +0100

    Removed papas on 2021 raffle

--- raffle.py
+++ raffle.py
@@ -1,19 +1,19 @@
 import random
 
 # Adults
-adults = ['Papa', 'Mama', 'Andres', 'Pablo', 'Clara', 'Jaime', 'Ana', 'MJ', 'Nacho', 'Marina']
-adults_exclusions = {'Pablo': ['Pablo', 'MJ'], 'Clara': ['Clara', 'Nacho'], 'Jaime': ['Jaime', 'Marina'], 'Papa': ['Papa', 'Mama'], 'Andres': ['Andres', 'Ana'],
-                        'MJ': ['MJ', 'Pablo'], 'Nacho': ['Nacho', 'Clara'], 'Marina': ['Marina', 'Jaime'], 'Mama': ['Mama', 'Papa'], 'Ana': ['Ana', 'Andres']}
+adults = ['Andres', 'Pablo', 'Clara', 'Jaime', 'Ana', 'MJ', 'Nacho', 'Marina']
+adults_exclusions = {'Pablo': ['Pablo', 'MJ'], 'Clara': ['Clara', 'Nacho'], 'Jaime': ['Jaime', 'Marina'], 'Andres': ['Andres', 'Ana'],
+                        'MJ': ['MJ', 'Pablo'], 'Nacho': ['Nacho', 'Clara'], 'Marina': ['Marina', 'Jaime'], 'Ana': ['Ana', 'Andres']}
 adults_results = {}
 
 # Pairs & Children
 children = {1: 'Nacho', 2: 'Elena', 3: 'Alonso', 4: 'Julia', 5: 'Irene', 6: 'Gonzalo'}
-pairs = ['Pablo & MJ', 'Clara & Nacho', 'Jaime & Marina', 'Papa & Mama', 'Andres & Ana']
-children_exclusions = {'Pablo & MJ': ['Elena', 'Irene'], 'Clara & Nacho': ['Nacho', 'Alonso'], 'Jaime & Marina': ['Julia', 'Gonzalo'], 'Papa & Mama': [], 'Andres & Ana': []}
+pairs = ['Pablo & MJ', 'Clara & Nacho', 'Jaime & Marina', 'Andres & Ana']
+children_exclusions = {'Pablo & MJ': ['Elena', 'Irene'], 'Clara & Nacho': ['Nacho', 'Alonso'], 'Jaime & Marina': ['Julia', 'Gonzalo'], 'Andres & Ana': []}
 children_results = {}
 
 def throwdice_adults():
-    dice = random.randint(0, 9)
+    dice = random.randint(0, 7)
     return adults[dice]
 
 def throwdice_children():
@@ -21,7 +21,7 @@ def throwdice_children():
     return children[dice]
 
 def throwdice_pairs():
-    dice = random.randint(0, 4)
+    dice = random.randint(0, 3)
     return pairs[dice]
 
 def run_adults():
@@ -42,6 +42,7 @@ def run_adults():
 
 def run_children():
     winners = []
+    winner_pairs = []
     # Loop through pairs, each will get a child assigned
     for pair in pairs:
         winner = throwdice_children()
@@ -66,14 +67,35 @@ def run_children():
         print(f'{winner} is excluded for pair {extra_pair}, throwing the dice again')
         winner = throwdice_children()
 
-    print(f'==> {extra_pair} assigned to {winner}')
+    winners.append(winner)
+    winner_pairs.append(extra_pair)
+    print(f'==> {extra_pair} extra assigned to {winner}')
     children_results.update({extra_pair + ' extra': winner})
+
+    # Another extra random pair gets a second child assigned
+    another_extra_pair = throwdice_pairs()
+    while another_extra_pair == extra_pair:
+        print(f'{another_extra_pair} is already assigned as extra, throwing the dice again')
+        another_extra_pair = throwdice_pairs()
+
+    winner = throwdice_children()
+    while winner in winners:
+        print(f'{winner} is already assigned, throwing the dice again')
+        winner = throwdice_children()
+    while winner in children_exclusions[another_extra_pair]:
+        print(f'{winner} is excluded for pair {another_extra_pair}, throwing the dice again')
+        winner = throwdice_children()
+
+    winners.append(winner)
+    winner_pairs.append(another_extra_pair)
+    print(f'==> {another_extra_pair} another assigned to {winner}')
+    children_results.update({another_extra_pair + ' another': winner})
 
 def run():
     run_adults()
     print(f'==> Adults Results: {adults_results}')
 
     run_children()
-    print(f' Children Results: {children_results}')
+    print(f'==> Children Results: {children_results}')
 
 run()
