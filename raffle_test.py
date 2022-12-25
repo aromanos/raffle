7b3f1f95799d221dc4a3c9b2d1de4e935c8ad7d6
andres <andres@andresromanos.com>
2022-12-25 21:57:02 +0100

    Added more tests

--- raffle_test.py
+++ raffle_test.py
@@ -4,10 +4,39 @@ import raffle
 
 class Tests(unittest.TestCase):
 
-    def test(self):
-        adults_results = raffle.run_adults()
-        print(adults_results)
-        self.assertEquals(len(adults_results), len(raffle.adults))
+    def test_number_of_adult_results(self):
+        results = raffle.run_adults()
+        self.assertEquals(len(results), len(raffle.adults))
+
+    def test_number_of_children_results(self):
+        results = raffle.run_children()
+        self.assertEquals(len(results), len(raffle.children))
+
+    def test_adult_assigned_to_jaime(self):
+        number_of_iterations = 100
+        for iter in range(number_of_iterations):
+            results = raffle.run_adults()
+            self.assertIn('Jaime', results)
+
+    def test_adult_exclusions(self):
+        number_of_iterations = 100
+        for iter in range(number_of_iterations):
+            results = raffle.run_adults()
+            self.assertNotEquals(results['Jaime'], 'Marina')
+            self.assertNotEquals(results['Jaime'], 'Jaime')
+
+    def test_children_exclusions(self):
+        number_of_iterations = 100
+        for iter in range(number_of_iterations):
+            results = raffle.run_children()
+            self.assertNotEquals(results['Jaime & Marina'], 'Julia')
+            self.assertNotEquals(results['Jaime & Marina'], 'Gonzalo')
+
+    def test_children_ana_andres(self):
+        number_of_iterations = 100
+        for iter in range(number_of_iterations):
+            results = raffle.run_children()
+            self.assertIn('Andres & Ana', results)
 
 if __name__ == '__main__':
     unittest.main()
